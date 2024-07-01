20dffe86580abff595f18d96a1e1f18ba02322ca
AntonijoTomic <antonijo.tomic12@gmail.com>
2024-07-01 20:43:11 +0200

    Add project files.

---
+++ DelDogzadatak/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DelDogzadatak
+{
+    internal class Program
+    {
+        public delegate void MatematickeOperacije(int a, int b);
+        static void Main(string[] args)
+        {
+
+            MatematickeOperacije delegatMatematicar = Zbroji;
+            delegatMatematicar += Pomnozi;
+            delegatMatematicar += Oduzmi;
+
+            delegatMatematicar.Invoke(10, 5);
+
+            Console.ReadLine();
+        
+        }
+        public static void Zbroji(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public static void Pomnozi(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+
+        public static void Oduzmi (int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+    }
+}
