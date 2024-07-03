00781ec04e21b5eae452757b922208fe852ce1db
AntonijoTomic <antonijo.tomic12@gmail.com>
2024-07-03 18:51:56 +0200

    drugi

--- DelDogzadatak/Program.cs
+++ DelDogzadatak/Program.cs
@@ -4,31 +4,20 @@ namespace DelDogzadatak
 {
     internal class Program
     {
-        public delegate void MatematickeOperacije(int a, int b);
+
         static void Main(string[] args)
         {
+            Utakmica utakmica = new Utakmica();
+            using (Rezultat rezultat = new Rezultat(utakmica))
+            {
+                // Simulacija golova
+                utakmica.GolDomaci();
+                utakmica.GolGost();
+                utakmica.GolDomaci();
 
-            MatematickeOperacije delegatMatematicar = Zbroji;
-            delegatMatematicar += Pomnozi;
-            delegatMatematicar += Oduzmi;
-
-            delegatMatematicar.Invoke(10, 5);
-
-            Console.ReadLine();
-        
-        }
-        public static void Zbroji(int a, int b)
-        {
-            Console.WriteLine(a + b);
-        }
-        public static void Pomnozi(int a, int b)
-        {
-            Console.WriteLine(a * b);
+                Console.ReadKey();
+            }
         }
 
-        public static void Oduzmi (int a, int b)
-        {
-            Console.WriteLine(a - b);
-        }
     }
 }
