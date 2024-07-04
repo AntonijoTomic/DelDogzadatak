b1460e18f4af241af7a84bdb2406a68a0501e5ab
AntonijoTomic <antonijo.tomic12@gmail.com>
2024-07-04 15:46:06 +0200

    action i func

--- DelDogzadatak/Program.cs
+++ DelDogzadatak/Program.cs
@@ -7,42 +7,32 @@ namespace DelDogzadatak
 {
     internal class Program
     {
+        static Action pozdrav = () =>
+        {
+            Console.WriteLine("Pozdrav! Akcijska metoda!");
+        };
+
+        
+        static Func<int, double> KorijenBroja= (broj) =>
+        {
+            return Math.Sqrt(broj);
+        };
 
         static void Main(string[] args)
         {
-           
 
-            List<Osoba> osobe = new List<Osoba>() {
-            new Osoba("Marko", "Marković", 10),
-            new Osoba("Ana", "Anić", 22),
-            new Osoba("Petar", "Petrović", 2),
-            new Osoba("Jelena", "Jelić", 24),
-            new Osoba("Ivan", "Ivanić", 32)};
+            pozdrav();
 
-            var Punoljetni = osobe.Where(o => o.Godine > 17);
-            var imeNaM = osobe.Where(o => o.Ime.StartsWith("M"));
-            foreach (var osoba in Punoljetni)
-            {
-                Console.WriteLine($"{osoba.Ime} {osoba.Prezime}, {osoba.Godine} godina");
-            }
-            Console.ReadLine();
+            // Pozivanje Func delegata
+            int broj = 25;
+            double korijen = KorijenBroja(broj);
+            Console.WriteLine($"Korijen broja '{broj}' u func metodi je {korijen}.");
+        
+
+        Console.ReadLine();
         }
         
     }
 
-    public class Osoba
-    {
-        // Polja (atributi) klase Osoba
-        public string Ime { get; set; }
-        public string Prezime { get; set; }
-        public int Godine { get; set; }
-
-        // Konstruktor klase Osoba
-        public Osoba(string ime, string prezime, int godine)
-        {
-            Ime = ime;
-            Prezime = prezime;
-            Godine = godine;
-        }
-    }
+  
 }
